de43edf60f28c3f3f9cc828b5a137eb8671a88bf
Allen Kim <allenkim95@gmail.com>
2018-03-28 13:28:54 -0400

    Add some css and some basic elements

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    http_accept = request.headers['Accept'] # accept-encoding accept-language
+    user_agent = request.headers['User-Agent']
+    return render_template('index.html', **locals())
 
 if __name__=='__main__':
     app.run(debug=True)
-
--- static/fing.js
+++ static/fing.js
@@ -1,6 +1,15 @@
 var table = document.getElementById('browser-info');
-var new_row = table.insertRow(table.rowIndex + 1);
 
-new_row.insertCell(0).innerHTML = "test";
-new_row.insertCell(1).innerHTML = "test2";
+function addRow(label, value){
+  var new_row = table.insertRow(table.rowIndex);
+  new_row.insertCell(0).innerHTML = label;
+  new_row.insertCell(1).innerHTML = value;
+}
 
+function getScreenResolution(){
+  return [window.screen.width, window.screen.height, window.screen.colorDepth]
+}
+
+addRow("Screen Size and Color Depth", getScreenResolution().join('x'));
+addRow("A","B")
+addRow("C","D")
