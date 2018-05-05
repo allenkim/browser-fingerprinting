5b7a464ec30ec81e485a942974c207f9c88fd02e
Shawn <sdude511@gmail.com>
2018-05-04 21:30:06 -0400

    more testing

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template, request
-from db import *
 
 app = Flask(__name__)
+
+try:
+    DATABASE_URL = os.environ['DATABASE_URL']
+except:
+    
+    DATABASE_URL = 'DATABASE_URL'
+def addRow(conn,name):
+    
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    sql = """INSERT INTO vendors(vendor_name) VALUES(%s)"""
+    name = "man"
+    cur = conn.cursor()
+    cur.execute(sql,(name,))
+    conn.comit()
+    conn.close()
+
+
+
+
 
 @app.route('/')
 def home():
