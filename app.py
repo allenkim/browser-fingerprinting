87efa3981a0a35d2f37e2a511648cd30b55ba355
Shawn <sdude511@gmail.com>
2018-05-07 00:04:36 -0400

    fix up some of the html

--- app.py
+++ app.py
@@ -31,16 +31,16 @@ def results():
         hvs = [i[0] for i in hvs]
         
         if hashVal not in hvs:
-            test = readID()
-            if len(test) == 0:
+            ids = readID()
+            if len(ids) == 0:
                 idVal = 0
             else:
-                test = [i[0] for i in test]
-                idVal = max(test)+1
+                ids = [i[0] for i in test]
+                idVal = max(ids)+1
             addRow2(idVal,platform,screen,lang,time,touch,cookie,fonts,canvas,webgl,hashVal)
         else:
             idVal = readFromHash(hashVal)[0][0]
 
-        return '{"visitor":' + str(idVal) + '}'
+            return '{"visitor":' + str(idVal) + ' You have been here before}'
 if __name__=='__main__':
     app.run(debug=True)
